675a46109144266d74ce1415f54ed65da9b2ba9a
Vishal Gudalkar <vishal.gudalkar@gmail.com>
2022-04-20 20:00:06 +0530

    conflict issue resolved

--- src/app/header/component/header.component.ts
+++ src/app/header/component/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AuthService } from 'src/app/login/services/auth.service';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { filter, map, Observable } from 'rxjs';
+import { AuthState, OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-header',
@@ -7,22 +10,32 @@ import { AuthService } from 'src/app/login/services/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  userName: string = "";
-  role: string = "prod";
-  key: string = "role";
-  //localStorage: any;
+  public name$!: Observable<string>;
+  public role$!: Observable<string>;
+  role: string = "";
+  key: string = "role"; 
   
-  constructor(private authService: AuthService) { }
+  constructor(private oktaAuthStateService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
   ngOnInit(): void {
-    this.authService.userName.subscribe(res => {
-      this.userName = res;
+    this.name$ = this.oktaAuthStateService.authState$.pipe(
+      filter((authState: AuthState) => !!authState && !!authState.isAuthenticated),
+      map((authState: AuthState) => authState.idToken?.claims.name ?? '')
+    );
+
+    this.role$ = this.oktaAuthStateService.authState$.pipe(
+      filter((authState: AuthState) => !!authState && !!authState.isAuthenticated),
+      map((authState: AuthState) => authState.idToken?.claims.groups[1] ?? '')
+    );
+
+    this.role$.subscribe(res => {
+      this.role = res;
     });
     localStorage.setItem(this.key, this.role);
   }
 
-  logout(){
-
+  async logout() {
+    await this.oktaAuth.signOut();
   }
 
 }
