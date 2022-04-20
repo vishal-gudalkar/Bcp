560506eea80d0aac03f34c520ecaca8da1d12bc7
Vishal Gudalkar <vishal.gudalkar@gmail.com>
2022-04-20 16:07:10 +0530

    issue resolved

--- src/app/login/components/login.component.ts
+++ src/app/login/components/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { AuthService } from '../services/auth.service';
 
 @Component({
